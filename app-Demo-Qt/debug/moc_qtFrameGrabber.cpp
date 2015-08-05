681ecac460b5b9f3bad63bcdd7fc4ad02557c4c8
hudrima1 <m.hudritsch@vtxmail.ch>
2015-08-05 17:35:26 +0200

    QCamera still doesn't work under Windows

--- app-Demo-Qt/debug/moc_qtFrameGrabber.cpp
+++ app-Demo-Qt/debug/moc_qtFrameGrabber.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'qtFrameGrabber.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'qtFrameGrabber.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.4.1. It"
+#error "This file was generated using the moc from 5.5.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,11 +20,11 @@
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_qtFrameGrabber_t {
     QByteArrayData data[8];
-    char stringdata[131];
+    char stringdata0[131];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_qtFrameGrabber_t, stringdata) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_qtFrameGrabber_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
 static const qt_meta_stringdata_qtFrameGrabber_t qt_meta_stringdata_qtFrameGrabber = {
@@ -76,6 +76,7 @@ void qtFrameGrabber::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
         qtFrameGrabber *_t = static_cast<qtFrameGrabber *>(_o);
+        Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->updateCameraState((*reinterpret_cast< QCamera::State(*)>(_a[1]))); break;
         case 1: _t->displayCameraError(); break;
@@ -99,7 +100,7 @@ const QMetaObject *qtFrameGrabber::metaObject() const
 void *qtFrameGrabber::qt_metacast(const char *_clname)
 {
     if (!_clname) return Q_NULLPTR;
-    if (!strcmp(_clname, qt_meta_stringdata_qtFrameGrabber.stringdata))
+    if (!strcmp(_clname, qt_meta_stringdata_qtFrameGrabber.stringdata0))
         return static_cast<void*>(const_cast< qtFrameGrabber*>(this));
     return QAbstractVideoSurface::qt_metacast(_clname);
 }
