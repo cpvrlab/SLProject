9ea5876c5854a7b90d26c0b69efbf6817f5aae20
hudrima1 <m.hudritsch@vtxmail.ch>
2015-08-05 16:50:16 +0200

    QCamera in app-Demo-Qt works on OSX with Qt 5.4

--- app-Demo-Qt/moc_qtFrameGrabber.cpp
+++ app-Demo-Qt/moc_qtFrameGrabber.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_qtFrameGrabber_t {
-    QByteArrayData data[1];
-    char stringdata0[15];
+    QByteArrayData data[8];
+    char stringdata0[131];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -29,10 +29,20 @@ struct qt_meta_stringdata_qtFrameGrabber_t {
     )
 static const qt_meta_stringdata_qtFrameGrabber_t qt_meta_stringdata_qtFrameGrabber = {
     {
-QT_MOC_LITERAL(0, 0, 14) // "qtFrameGrabber"
+QT_MOC_LITERAL(0, 0, 14), // "qtFrameGrabber"
+QT_MOC_LITERAL(1, 15, 17), // "updateCameraState"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 14), // "QCamera::State"
+QT_MOC_LITERAL(4, 49, 18), // "displayCameraError"
+QT_MOC_LITERAL(5, 68, 16), // "updateLockStatus"
+QT_MOC_LITERAL(6, 85, 19), // "QCamera::LockStatus"
+QT_MOC_LITERAL(7, 105, 25) // "QCamera::LockChangeReason"
 
     },
-    "qtFrameGrabber"
+    "qtFrameGrabber\0updateCameraState\0\0"
+    "QCamera::State\0displayCameraError\0"
+    "updateLockStatus\0QCamera::LockStatus\0"
+    "QCamera::LockChangeReason"
 };
 #undef QT_MOC_LITERAL
 
@@ -42,22 +52,58 @@ static const uint qt_meta_data_qtFrameGrabber[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   29,    2, 0x08 /* Private */,
+       4,    0,   32,    2, 0x08 /* Private */,
+       5,    2,   33,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    2,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 6, 0x80000000 | 7,    2,    2,
+
        0        // eod
 };
 
 void qtFrameGrabber::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        qtFrameGrabber *_t = static_cast<qtFrameGrabber *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->updateCameraState((*reinterpret_cast< QCamera::State(*)>(_a[1]))); break;
+        case 1: _t->displayCameraError(); break;
+        case 2: _t->updateLockStatus((*reinterpret_cast< QCamera::LockStatus(*)>(_a[1])),(*reinterpret_cast< QCamera::LockChangeReason(*)>(_a[2]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QCamera::State >(); break;
+            }
+            break;
+        case 2:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 1:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QCamera::LockChangeReason >(); break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QCamera::LockStatus >(); break;
+            }
+            break;
+        }
+    }
 }
 
 const QMetaObject qtFrameGrabber::staticMetaObject = {
@@ -84,6 +130,15 @@ int qtFrameGrabber::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QAbstractVideoSurface::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
