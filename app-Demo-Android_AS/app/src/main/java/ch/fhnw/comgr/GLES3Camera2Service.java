d574e6caf170400ea91f06093c30904432bad66b
Marcus Hudritsch <marcus.hudritsch@bfh.ch>
2017-02-08 17:19:53 +0100

    …

---
+++ app-Demo-Android_AS/app/src/main/java/ch/fhnw/comgr/GLES3Camera2Service.java
@@ -0,0 +1,163 @@
+package ch.fhnw.comgr;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.Arrays;
+
+@SuppressWarnings("MissingPermission")
+public class GLES3Camera2Service extends Service {
+    protected static final String TAG = "SLProject";
+    protected static final int CAMERACHOICE = CameraCharacteristics.LENS_FACING_BACK;
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession session;
+    protected ImageReader imageReader;
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand flags " + flags + " startId " + startId);
+
+        CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            String pickedCamera = getCamera(manager);
+            manager.openCamera(pickedCamera, cameraStateCallback, null);
+            imageReader = ImageReader.newInstance(640, 480, ImageFormat.YUV_420_888, 2 /* images buffered */);
+            imageReader.setOnImageAvailableListener(onImageAvailableListener, null);
+            Log.i(TAG, "imageReader created");
+        } catch (CameraAccessException e){
+            Log.e(TAG, e.getMessage());
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    // Return the Camera Id which matches the field CAMERACHOICE
+    public String getCamera(CameraManager manager) {
+        try {
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                int cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cOrientation == CAMERACHOICE) {
+                    return cameraId;
+                }
+            }
+        } catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            Log.i(TAG, "CameraDevice.StateCallback onOpened");
+            cameraDevice = camera;
+            actOnReadyCameraDevice();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            Log.w(TAG, "CameraDevice.StateCallback onDisconnected");
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.e(TAG, "CameraDevice.StateCallback onError " + error);
+        }
+    };
+
+    protected CameraCaptureSession.StateCallback sessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            Log.i(TAG, "CameraCaptureSession.StateCallback onConfigured");
+            GLES3Camera2Service.this.session = session;
+            try {
+                session.setRepeatingRequest(createCaptureRequest(), null, null);
+            } catch (CameraAccessException e){
+                Log.e(TAG, e.getMessage());
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {}
+    };
+
+    protected ImageReader.OnImageAvailableListener onImageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image img = reader.acquireLatestImage();
+
+            if (img == null)
+                return;
+
+            // Check image format
+            int format = reader.getImageFormat();
+            if (format != ImageFormat.YUV_420_888) {
+                throw new IllegalArgumentException("Camera image must have format YUV_420_888.");
+            }
+
+            Image.Plane[] planes = img.getPlanes();
+            // Spec guarantees that planes[0] is luma and has pixel stride of 1.
+            // It also guarantees that planes[1] and planes[2] have the same row and pixel stride.
+            if (planes[1].getPixelStride() != 1 && planes[1].getPixelStride() != 2) {
+                throw new IllegalArgumentException(
+                        "src chroma plane must have a pixel stride of 1 or 2: got "
+                                + planes[1].getPixelStride());
+            }
+
+            GLES3Lib.copyVideoImage(img.getWidth(), img.getHeight(), planes[0].getBuffer());
+
+            img.close();
+        }
+    };
+
+
+
+    public void actOnReadyCameraDevice()
+    {
+        try {
+            cameraDevice.createCaptureSession(Arrays.asList(imageReader.getSurface()), sessionStateCallback, null);
+        } catch (CameraAccessException e){
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        try {
+            session.abortCaptures();
+        } catch (CameraAccessException e){
+            Log.e(TAG, e.getMessage());
+        }
+        session.close();
+    }
+
+    protected CaptureRequest createCaptureRequest() {
+        try {
+            CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+            builder.addTarget(imageReader.getSurface());
+            return builder.build();
+        } catch (CameraAccessException e) {
+            Log.e(TAG, e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
