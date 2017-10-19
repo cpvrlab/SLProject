5b4f304fe90f4db5c8bb53b2fd9712a529cf132c
ghm1 <michael.goettlicher@bfh.ch>
2017-10-19 13:28:30 +0200

    fixed compiler error.

--- lib-SLProject/source/CV/SLCVRaulMurOrb.cpp
+++ lib-SLProject/source/CV/SLCVRaulMurOrb.cpp
@@ -908,9 +908,9 @@ void SLCVRaulMurOrb::ComputePyramid(SLCVMat image)
     }
 
     //save image pyriamid
-    for (int level = 0; level < nlevels; ++level) {
-        string filename = "D:/Development/SLProject/debug_ouput/imagePyriamid" + std::to_string(level) + ".jpg";
+    /*for (int level = 0; level < nlevels; ++level) {
+        string filename = "D:/Development/SLProject/debug_ouput/imagePyriamid" + to_string(level) + ".jpg";
         cv::imwrite(filename, mvImagePyramid[level]);
-    }
+    }*/
 }
 //-----------------------------------------------------------------------------
