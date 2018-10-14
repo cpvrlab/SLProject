3b8d0daa01e07f329586285bc0ab1774cba23c9e
Marcus Hudritsch <marcus.hudritsch@bfh.ch>
2018-10-14 13:06:45 +0200

    Fixed bug that came by changing order in SLTransformSpace

--- include/SLNode.h
+++ include/SLNode.h
@@ -204,14 +204,27 @@ class SLNode
     // transform setter methods
     void translation(const SLVec3f&   pos,
                      SLTransformSpace relativeTo = TS_parent);
-    void translation(SLfloat x, SLfloat y, SLfloat z, SLTransformSpace relativeTo = TS_parent);
+    void translation(SLfloat          x,
+                     SLfloat          y,
+                     SLfloat          z,
+                     SLTransformSpace relativeTo = TS_parent);
     void rotation(const SLQuat4f&  rot,
                   SLTransformSpace relativeTo = TS_parent);
-    void rotation(SLfloat angleDeg, const SLVec3f& axis, SLTransformSpace relativeTo = TS_parent);
+    void rotation(SLfloat          angleDeg,
+                  const SLVec3f&   axis,
+                  SLTransformSpace relativeTo = TS_parent);
     void scaling(SLfloat s);
-    void scaling(SLfloat x, SLfloat y, SLfloat z);
+    void scaling(SLfloat x,
+                 SLfloat y,
+                 SLfloat z);
     void scaling(const SLVec3f& scaling);
-    void lookAt(SLfloat targetX, SLfloat targetY, SLfloat targetZ, SLfloat upX = 0, SLfloat upY = 1, SLfloat upZ = 0, SLTransformSpace relativeTo = TS_world);
+    void lookAt(SLfloat          targetX,
+                SLfloat          targetY,
+                SLfloat          targetZ,
+                SLfloat          upX        = 0,
+                SLfloat          upY        = 1,
+                SLfloat          upZ        = 0,
+                SLTransformSpace relativeTo = TS_world);
     void lookAt(const SLVec3f&   target,
                 const SLVec3f&   up         = SLVec3f::AXISY,
                 SLTransformSpace relativeTo = TS_world);
@@ -219,10 +232,15 @@ class SLNode
     // transform modifier methods
     void translate(const SLVec3f&   vec,
                    SLTransformSpace relativeTo = TS_object);
-    void translate(SLfloat x, SLfloat y, SLfloat z, SLTransformSpace relativeTo = TS_object);
+    void translate(SLfloat          x,
+                   SLfloat          y,
+                   SLfloat          z,
+                   SLTransformSpace relativeTo = TS_object);
     void rotate(const SLQuat4f&  rot,
                 SLTransformSpace relativeTo = TS_object);
-    void rotate(SLfloat angleDeg, const SLVec3f& axis, SLTransformSpace relativeTo = TS_object);
+    void rotate(SLfloat          angleDeg,
+                const SLVec3f&   axis,
+                SLTransformSpace relativeTo = TS_object);
     void rotate(SLfloat          angleDeg,
                 SLfloat          x,
                 SLfloat          y,
