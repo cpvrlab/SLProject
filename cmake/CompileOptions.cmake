a02247e54487ee0f915fac96669d666860f8d3b9
Marcus Hudritsch <marcus.hudritsch@bfh.ch>
2018-10-15 14:32:01 +0200

    Both executable have now an extension -debug or -release

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -14,8 +14,8 @@ endif()
 
 
 set(DEFAULT_PROJECT_OPTIONS
-    DEBUG_POSTFIX             "d"
-    RELEASE_POSTFIX           "r"
+    DEBUG_POSTFIX             "-debug"
+    RELEASE_POSTFIX           "-release"
     CXX_STANDARD              14
     LINKER_LANGUAGE           "CXX"
     POSITION_INDEPENDENT_CODE ON
@@ -51,16 +51,9 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
         /MP           # -> build with multiple processes
         /W4           # -> warning level 4
-        # /WX         # -> treat warnings as errors
 
         /wd4251       # -> disable warning: 'identifier': class 'type' needs to have dll-interface to be used by clients of class 'type2'
         /wd4592       # -> disable warning: 'identifier': symbol will be dynamically initialized (implementation limitation)
-        # /wd4201     # -> disable warning: nonstandard extension used: nameless struct/union (caused by GLM)
-        # /wd4127     # -> disable warning: conditional expression is constant (caused by Qt)
-        
-        #$<$<CONFIG:Debug>:
-        #/RTCc         # -> value is assigned to a smaller data type and results in a data loss
-        #>
 
         $<$<CONFIG:Release>: 
         /Gw           # -> whole program global optimization
