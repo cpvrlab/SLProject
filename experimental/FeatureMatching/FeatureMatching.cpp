86050a7abafb30276119caf7aa40c59f5ea9f9c2
ghm1 <ghm1@bfh.ch>
2019-09-13 08:58:13 +0200

    *Enabled app-Demo-orb-extractor and FeatureMatching apps for windows. Updated them for opencv4. *CMakeSettings: added Debug and RelWithDebInfo configuration for direct CMake folder usage in visual studio.

--- experimental/FeatureMatching/FeatureMatching.cpp
+++ experimental/FeatureMatching/FeatureMatching.cpp
@@ -10,34 +10,34 @@ int main(int argc, char** argv)
 
     if (argc < 3)
     {
-        std::cout << "Usage:" << std::endl << argv[0] << " image1 image2" << std::endl;
+        std::cout << "Usage:" << std::endl
+                  << argv[0] << " image1 image2" << std::endl;
         exit(1);
     }
 
-    app.name = "Best tool ever made!";
-    app.closeup_left = "closeup left";
+    app.name          = "Best tool ever made!";
+    app.closeup_left  = "closeup left";
     app.closeup_right = "closeup right";
     app.select_radius = 10;
-    app.local_idx = 0;
-    app.right_idx = 0;
-    app.left_idx = 0;
-    app.method = STOCK_ORBSLAM;
+    app.local_idx     = 0;
+    app.right_idx     = 0;
+    app.left_idx      = 0;
+    app.method        = STOCK_ORBSLAM;
 
-    app.image1 = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    app.image2 = cv::imread(argv[2], CV_LOAD_IMAGE_COLOR);
+    app.image1 = cv::imread(argv[1], cv::IMREAD_COLOR);
+    app.image2 = cv::imread(argv[2], cv::IMREAD_COLOR);
 
     if (!app.image1.data || !app.image2.data)
     {
-        std::cout << "Usage:" << std::endl << argv[0] << " image1 image2" << std::endl;
+        std::cout << "Usage:" << std::endl
+                  << argv[0] << " image1 image2" << std::endl;
         std::cout << "Can't open images" << std::endl;
         exit(1);
     }
 
     app_prepare(app);
-    
+
     start_gui(app);
 
     return 0;
 }
-
-
