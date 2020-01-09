bbf095af73c031b8aafbc7f79d4ba809dc1cf87b
ghm1 <ghm1@bfh.ch>
2020-01-09 13:39:43 +0100

    *Definition of SENSFrame that wraps gray and rgb frame together with adjustments made. *removed dependency to CVCapture from native app

--- apps/source/sens/SENSCamera.h
+++ apps/source/sens/SENSCamera.h
@@ -3,16 +3,50 @@
 
 #include <opencv2/core.hpp>
 
+struct SENSFrame
+{
+    SENSFrame(cv::Mat imgRGB,
+              cv::Mat imgGray,
+              int     captureWidth,
+              int     captureHeight,
+              int     cropW,
+              int     cropH,
+              bool    mirroredH,
+              bool    mirroredV)
+            : imgRGB(imgRGB),
+              imgGray(imgGray),
+              captureWidth(captureWidth),
+              captureHeight(captureHeight),
+              cropW(cropW),
+              cropH(cropH),
+              mirroredH(mirroredH),
+              mirroredV(mirroredV)
+    {
+    }
+
+    cv::Mat imgRGB;
+    cv::Mat imgGray;
+
+    const int  captureWidth;
+    const int  captureHeight;
+    const int  cropW;
+    const int  cropH;
+    const bool mirroredH;
+    const bool mirroredV;
+};
+typedef std::shared_ptr<SENSFrame> SENSFramePtr;
+
+
 class SENSCamera
 {
 public:
     enum class Facing
     {
-        FRONT,
+        FRONT = 0,
         BACK
     };
 
-    enum class FocusMode : int32_t
+    enum class FocusMode
     {
         CONTINIOUS_AUTO_FOCUS = 0,
         FIXED_INFINITY_FOCUS
@@ -25,7 +59,7 @@ public:
 
     virtual void    start(int width, int height, FocusMode focusMode) = 0;
     virtual void    stop(){};
-    virtual cv::Mat getLatestFrame() = 0;
+    virtual SENSFramePtr getLatestFrame() = 0;
 
 private:
     SENSCamera::Facing _facing;
