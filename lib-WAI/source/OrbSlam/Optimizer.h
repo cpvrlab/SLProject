4f586c2383a8ccc327d020d72b9c9ed090f5ea17
luc <luc.girod@bfh.ch>
2020-02-04 13:10:37 +0100

    optimizer set MapPoint to null

--- lib-WAI/source/OrbSlam/Optimizer.h
+++ lib-WAI/source/OrbSlam/Optimizer.h
@@ -41,6 +41,8 @@ public:
     void static BundleAdjustment(const std::vector<WAIKeyFrame*>& vpKF, const std::vector<WAIMapPoint*>& vpMP, int nIterations = 5, bool* pbStopFlag = NULL, const unsigned long nLoopKF = 0, const bool bRobust = true);
     void static GlobalBundleAdjustemnt(WAIMap* pMap, int nIterations = 5, bool* pbStopFlag = NULL, const unsigned long nLoopKF = 0, const bool bRobust = true);
     void static LocalBundleAdjustment(WAIKeyFrame* pKF, bool* pbStopFlag, WAIMap* pMap);
+
+    int static PoseOptimization(WAIFrame* pFrame, vector<bool> &vbOutliers);
     int static PoseOptimization(WAIFrame* pFrame);
 
     // if bFixScale is true, 6DoF optimization (stereo,rgbd), 7DoF otherwise (mono)
