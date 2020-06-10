039bff1649c06b24f468412837396e4a8e29000c
Michael Goettlicher <michael.goettlicher@bfh.ch>
2020-06-10 10:35:47 +0200

    *cleaned ErlebAR view controller code

--- apps/app-Demo-WAI/iOS/ErlebARViewController.h
+++ apps/app-Demo-WAI/iOS/ErlebARViewController.h
@@ -1,10 +1,13 @@
-//
-//  ErlebARViewController.h
-//  comgr
-//
-//  Created by Marcus Hudritsch on 30.11.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+//#############################################################################
+//  File:      ErlebARViewController.h
+//  Purpose:
+//  Author:    Michael Göttlicher
+//  Date:      June 2020
+//  Codestyle: https://github.com/cpvrlab/SLProject/wiki/SLProject-Coding-Style
+//  Copyright: Marcus Hudritsch
+//             This software is provide under the GNU General Public License
+//             Please visit: http://opensource.org/licenses/GPL-3.0
+//#############################################################################
 
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
