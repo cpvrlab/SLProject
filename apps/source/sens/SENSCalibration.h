516479b435fd833e7a227c48fabbc4977d73af2e
Michael Goettlicher <michael.goettlicher@bfh.ch>
2020-12-14 18:56:26 +0100

    *fixed merge error

--- apps/source/sens/SENSCalibration.h
+++ apps/source/sens/SENSCalibration.h
@@ -77,11 +77,7 @@ public:
     {
     }
 
-<<<<<<< HEAD
     //! copy constructor
-=======
-    //copy constructor
->>>>>>> template-camera-correction
     SENSCalibration(const SENSCalibration& other)
       : _cameraMat(other._cameraMat.clone()),
         _distortion(other._distortion.clone()),
@@ -222,15 +218,9 @@ public:
     void calculateUndistortedCameraMat();
 
 private:
-<<<<<<< HEAD
     bool load(const string& calibDir,
               const string& calibFileName,
               bool          calcUndistortionMaps);
-=======
-    bool load(const std::string& calibDir,
-              const std::string& calibFileName,
-              bool               calcUndistortionMaps);
->>>>>>> template-camera-correction
 
     void createFromGuessedFOV(int imageWidthPX, int imageHeightPX, float fovH);
 
