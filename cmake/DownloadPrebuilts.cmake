914c894b467616d0cb9e63bff69de425e8c88538
luc <luc@gluc.ch>
2020-12-16 17:35:05 +0100

    static openssl

--- cmake/DownloadPrebuilts.cmake
+++ cmake/DownloadPrebuilts.cmake
@@ -343,15 +343,13 @@ elseif("${SYSTEM_NAME_UPPER}" STREQUAL "WINDOWS") #-----------------------------
     endif ()
     link_directories(${openssl_LINK_DIR})
 
-    add_library(crypto SHARED IMPORTED)
-    add_library(ssl SHARED IMPORTED)
+    add_library(crypto STATIC IMPORTED)
+    add_library(ssl STATIC IMPORTED)
     set_target_properties(crypto PROPERTIES
-        IMPORTED_IMPLIB "${openssl_LINK_DIR}/libcrypto_static.lib"
-        IMPORTED_LOCATION "${openssl_LINK_DIR}/libcrypto-3.dll"
+        IMPORTED_LOCATION "${openssl_LINK_DIR}/libcrypto_static.lib"
     )
     set_target_properties(ssl PROPERTIES
-        IMPORTED_IMPLIB "${openssl_LINK_DIR}/libssl_static.lib"
-        IMPORTED_LOCATION "${openssl_LINK_DIR}/libssl-3.dll"
+        IMPORTED_LOCATION "${openssl_LINK_DIR}/libssl_static.lib"
     )
 
     ######################
