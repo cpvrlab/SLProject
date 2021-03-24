e6ae68d3cb8c7268e4465aa9e65946fb0fbb0dde
Göttlicher Michael <michael.goettlicher@bfh.ch>
2021-03-24 09:39:39 +0100

    *build zstd scripts for mac, android and ios

--- externals/prebuild_scripts/build_zstd_for_andV8.sh
+++ externals/prebuild_scripts/build_zstd_for_andV8.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+#ATTENTION: change toolchain path for your computer
 
 VERSION=v1.4.9-cpvr
-BUILD_R=BUILD_MACOS_RELEASE_"$VERSION"
-
+BUILD_R=BUILD_ANDROID_RELEASE_"$VERSION"
+TOOLCHAIN_PATH=/Users/ghm1/Library/Android/sdk/ndk/21.3.6528147/build/cmake/android.toolchain.cmake
 echo "============================================================"
-echo "Cloning zstd Version: $VERSION DEBUG"
+echo "Cloning zstd Version: $VERSION"
 echo "============================================================"
 
 if [ ! -d "zstd/.git" ]; then
@@ -20,8 +21,14 @@ echo "Building Release"
 echo "============================================================"
 
 cd build
+#rm -rf $BUILD_R
 mkdir $BUILD_R
 cd $BUILD_R
 
-cmake ../cmake -GXcode -DCMAKE_INSTALL_PREFIX=./install -DZSTD_BUILD_TESTS=OFF -DZSTD_BUILD_PROGRAMS=OFF
+cmake ../cmake \
+	-DCMAKE_INSTALL_PREFIX=./install \
+	-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH \
+    -DANDROID_ABI=arm64-v8a \
+    -DZSTD_BUILD_TESTS=OFF \
+    -DZSTD_BUILD_PROGRAMS=OFF
 cmake --build . --config Release --target install
