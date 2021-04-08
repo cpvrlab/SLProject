0536fb381620399ff0bd496734456096f3a4faec
Marcus Hudritsch <marcus.hudritsch@bfh.ch>
2021-04-08 09:10:57 +0200

    Updated OpenCV to 4.5.2 for Mac64

--- cmake/DownloadPrebuilts.cmake
+++ cmake/DownloadPrebuilts.cmake
@@ -486,9 +486,10 @@ elseif("${SYSTEM_NAME_UPPER}" STREQUAL "DARWIN" AND
     ###########################
 
     # Now download for MacOS
-    #set(OpenCV_VERSION "4.1.1")
 	#set(OpenCV_VERSION "3.4.1")
-    set(OpenCV_VERSION "4.5.0")
+    #set(OpenCV_VERSION "4.1.1")
+    #set(OpenCV_VERSION "4.5.0")
+    set(OpenCV_VERSION "4.5.2")
     set(OpenCV_PREBUILT_DIR "mac64_opencv_${OpenCV_VERSION}")
     set(OpenCV_DIR "${PREBUILT_PATH}/${OpenCV_PREBUILT_DIR}")
     set(OpenCV_INCLUDE_DIR "${OpenCV_DIR}/include")
