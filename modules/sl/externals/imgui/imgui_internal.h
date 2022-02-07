acb0a8472d361f8bfa42c8f85de25ddbd0775d17
Marcus Hudritsch <marcus.hudritsch@bfh.ch>
2022-02-07 09:05:22 +0100

    Update imgui_internal.h
    Ignore invalid-noreturn warning in imgui code.

--- modules/sl/externals/imgui/imgui_internal.h
+++ modules/sl/externals/imgui/imgui_internal.h
@@ -82,6 +82,7 @@ Index of this file:
 #pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
 #pragma clang diagnostic ignored "-Wdouble-promotion"
 #pragma clang diagnostic ignored "-Wimplicit-int-float-conversion"  // warning: implicit conversion from 'xxx' to 'float' may lose precision
+#pragma clang diagnostic ignored "-Winvalid-noreturn"
 #elif defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpragmas"              // warning: unknown option after '#pragma GCC diagnostic' kind
