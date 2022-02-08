909715e49eaab1389707458c35273c82a3bddfa4
Chnoblouch <m.v.wattenwyl@gmail.com>
2022-02-08 15:12:22 +0100

    Add support for Clang on Windows
    It's now possible to compile SLProject using clang-cl on Windows. clang-cl is supposed to be a "drop-in replacement" for the MSVC compiler, but there were still some flags that needed to be set. In addition, the CMakeLists were updated to account for a Clang compiler with an MSVC frontend (as opposed to a GNU frontend)

--- cmake/ConfigureMSVCCLion.cmake
+++ cmake/ConfigureMSVCCLion.cmake
@@ -2,10 +2,16 @@
 # Set the output path for the executable to the directory where the DLLs are located
 #
 
-if ("$ENV{CLION_IDE}" AND "${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
+if (
+    "$ENV{CLION_IDE}" AND                                                                           # CLion IDE
+    ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC" OR                                                   # cl compiler
+    ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang" AND "${CMAKE_CXX_SIMULATE_ID}" MATCHES "MSVC"))     # clang-cl compiler
+)
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
         set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/Debug")
     elseif (CMAKE_BUILD_TYPE STREQUAL "Release")
         set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/Release")
-    endif()
-endif()
+    elseif (CMAKE_BUILD_TYPE STREQUAL "RelWithDebugInfo")
+        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/RelWithDebugInfo")
+    endif ()
+endif ()
