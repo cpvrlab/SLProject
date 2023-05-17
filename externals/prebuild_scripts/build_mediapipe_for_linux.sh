0962313544f7bd73c79ae3b09302c8a3e2ae3b5b
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2023-05-17 17:18:20 +0200

    [MediaPipe] Build release version on Linux

--- externals/prebuild_scripts/build_mediapipe_for_linux.sh
+++ externals/prebuild_scripts/build_mediapipe_for_linux.sh
@@ -5,10 +5,10 @@
 # ####################################################
 
 VERSION=v0.8.11
-OPENCV_VERSION=4.5.5
+OPENCV_VERSION=4.7.0
 
 # This will break if the OpenCV script is changed
-OPENCV_DIR="opencv/build/linux_debug_$OPENCV_VERSION/install"
+OPENCV_DIR="opencv/build/linux_release_$OPENCV_VERSION/install"
 
 PREBUILT_NAME=linux_mediapipe_$VERSION
 PREBUILT_DIR="../prebuilt/$PREBUILT_NAME"
@@ -18,13 +18,23 @@ clear
 
 if [ ! -d "$OPENCV_DIR" ]; then ./build_opencv_w_contrib_for_linux.sh "$OPENCV_VERSION"; fi
 
+rm -rf "$PREBUILT_DIR"
+rm -rf "$DATA_DIR"
+mkdir "$PREBUILT_DIR"
+mkdir "$PREBUILT_DIR/debug"
+mkdir "$PREBUILT_DIR/release"
+
 if [ ! -d libmediapipe ]; then git clone https://github.com/cpvrlab/libmediapipe.git; fi
 cd libmediapipe
 ./build-x86_64-linux.sh --version $VERSION --config debug --opencv_dir "../$OPENCV_DIR"
 cd ..
 
-rm -rf "$PREBUILT_DIR"
-cp -r "libmediapipe/output/libmediapipe-$VERSION-x86_64-linux" "$PREBUILT_DIR"
-
-rm -rf "$DATA_DIR"
+cp -r "libmediapipe/output/libmediapipe-$VERSION-x86_64-linux/include" "$PREBUILT_DIR/include"
+cp -r "libmediapipe/output/libmediapipe-$VERSION-x86_64-linux/lib/"* "$PREBUILT_DIR/debug"
 cp -r "libmediapipe/output/data/mediapipe" "$DATA_DIR"
+
+cd libmediapipe
+./build-x86_64-linux.sh --version $VERSION --config release --opencv_dir "../$OPENCV_DIR"
+cd ..
+
+cp -r "libmediapipe/output/libmediapipe-$VERSION-x86_64-linux/lib/"* "$PREBUILT_DIR/release"
