b02a79c729e0758126e05921df084c8c8ad3219d
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2023-05-24 12:32:50 +0200

    Set up cmake-format

--- scripts/cmake_format_all.sh
+++ scripts/cmake_format_all.sh
@@ -1,7 +1,11 @@
 # runs cmake-format recursively on defined directories
 # (you have to install cmake-format from https://github.com/cheshirekow/cmake_format)
 
-find ../modules/test0 ../modules/testX \
+SL_ROOT="$(dirname -- "$0")/.."
+SEARCH_PATHS="$SL_ROOT"
+
+find $SEARCH_PATHS \
 	-type f \( -iname CMakeLists.txt -o -iname \*.cmake \) \
 	-not -path '*externals*' \
+	-not -path '*experimental*' \
 	-exec cmake-format -i {} \;
