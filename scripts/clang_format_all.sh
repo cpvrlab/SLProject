1b326f246b6ab333aafb1efe9718cdd826476126
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2023-05-24 11:04:49 +0200

    Update clang_format_all.sh to format everything
    ...except externals

--- scripts/clang_format_all.sh
+++ scripts/clang_format_all.sh
@@ -1,6 +1,10 @@
 # runs clang-format recursively on defined directories
 
-find ../modules/test0 ../modules/testX \
+SL_ROOT="$(dirname -- "$0")/.."
+SEARCH_PATHS="$SL_ROOT/modules/*/source $SL_ROOT/apps"
+
+find $SEARCH_PATHS \
 	-type f \( -iname \*.cpp -o -iname \*.h -o -iname \*.hpp -o -iname \*.mm -o -iname \*.m \) \
 	-not -path '*externals*' \
+	-not -path '*orb_slam*' \
 	-exec clang-format -i {} \;
